92dd67ec45da7aff4e5e6de0c7ad9899b4b1d9b4
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-18 22:51:09 +0200

    feat(user): add user creator service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserPutController } from './controllers/user-put.controller';
+import { UserCreator } from './services/user-creator.service';
 
 @Module({
   controllers: [UserPutController],
-  providers: [],
+  providers: [UserCreator],
 })
 export class UsersModule {}
