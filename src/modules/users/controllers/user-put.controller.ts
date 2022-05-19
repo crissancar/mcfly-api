2538801a6f00c2ccf38a3f318f7e494c17789088
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-19 20:31:43 +0200

    feat(controller): catch user already exists exception

--- src/modules/users/controllers/user-put.controller.ts
+++ src/modules/users/controllers/user-put.controller.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 import { CreateUserRequest } from '../dtos/create-user-request.dto';
 import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { UserCreator } from '../services/user-creator.service';
+import { UserAlreadyExists } from '../exceptions/user-already-exists.exception';
 
 @ApiTags('users')
 @Controller('users')
@@ -12,7 +13,15 @@ export class UserPutController {
   @Put(':id')
   @ApiCreatedResponse()
   async run(@Param('id') id: string, @Body() req: CreateUserRequest, @Res() res: Response) {
-    await this.creator.run({ id, ...req });
+    try {
+      await this.creator.run({ id, ...req });
+    } catch (error) {
+      if (error instanceof UserAlreadyExists) {
+        res.status(HttpStatus.BAD_REQUEST).send();
+      } else {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+      }
+    }
 
     res.status(HttpStatus.CREATED).send();
   }
