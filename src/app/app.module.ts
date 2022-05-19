a1af0559f70dd036763a230408e931642357d1cd
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-19 18:58:24 +0200

    refactor(module): remove app controller and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from '../modules/users/users.module';
 import { MongooseConfigModule } from '../config/persistence/mongoose-config.module';
 
 @Module({
   imports: [UsersModule, MongooseConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
