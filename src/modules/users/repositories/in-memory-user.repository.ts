afc2bd43e3c51dd9b1d2e5a8126f32e21ddde888
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-20 00:13:25 +0200

    feat(user): add find user use case in repositories

--- src/modules/users/repositories/in-memory-user.repository.ts
+++ src/modules/users/repositories/in-memory-user.repository.ts
@@ -1,10 +1,15 @@
 import { UserRepository } from './user.repository';
 import { User } from '../models/user.model';
+import { Nullable } from 'src/modules/shared/types/Nullable';
 
 export class InMemoryUserRepository implements UserRepository {
   save(user: User): Promise<void> {
     console.log(user);
 
     return Promise.resolve();
+  }
+
+  search(id: string): Promise<Nullable<User>> {
+    return Promise.resolve(undefined);
   }
 }
--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -1,5 +1,7 @@
 import { User } from '../models/user.model';
+import { Nullable } from '../../shared/types/Nullable';
 
 export interface UserRepository {
   save(user: User): Promise<void>;
+  search(id: string): Promise<Nullable<User>>;
 }
