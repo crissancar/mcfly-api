7b1dfb7b6651d5ef05b9fe86e476a6773adeb859
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-19 20:33:51 +0200

    feat(service): catch mongoose duplicate key error

--- src/modules/users/services/user-creator.service.ts
+++ src/modules/users/services/user-creator.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserRequest } from '../dtos/create-user-request.dto';
 import { User } from '../models/user.model';
 import { UserRepository } from '../repositories/user.repository';
+import { UserAlreadyExists } from '../exceptions/user-already-exists.exception';
 
 @Injectable()
 export class UserCreator {
@@ -10,6 +11,16 @@ export class UserCreator {
   async run(request: CreateUserRequest): Promise<void> {
     const user = User.create(request.id, request.username, request.email, request.password);
 
-    await this.repository.save(user);
+    try {
+      await this.repository.save(user);
+    } catch (error) {
+      if (this.checkMongooseUniqueEmailError(error)) {
+        throw new UserAlreadyExists(user.email);
+      }
+    }
+  }
+
+  private checkMongooseUniqueEmailError(error): boolean {
+    return error.name === 'MongoServerError' && error.code === 11000;
   }
 }
