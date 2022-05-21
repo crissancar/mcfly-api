8f89e3da2d698d89657c1f407a2b83f6d44a053e
Cristian <63931867+crissancar@users.noreply.github.com>
2022-05-22 01:27:00 +0200

    Feature/deactivate user use case (#6)

--- src/modules/activated-users/repositories/activated-users.repository.ts
+++ src/modules/activated-users/repositories/activated-users.repository.ts
@@ -2,4 +2,5 @@ import { ActivatedUser } from '../models/activated-user.model';
 
 export interface ActivatedUsersRepository {
   save(activatedUser: ActivatedUser): Promise<void>;
+  delete(activatedUser: ActivatedUser): Promise<void>;
 }
