9679f0ad4fa2b922234c4f52faadc887135fb083
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-21 23:04:15 +0200

    refactor(modules): update modules config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../modules/users/users.module';
 import { MongooseConfigModule } from '../config/persistence/mongoose-config.module';
+import { AuthModule } from '../modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MongooseConfigModule],
+  imports: [UsersModule, AuthModule, MongooseConfigModule],
 })
 export class AppModule {}
