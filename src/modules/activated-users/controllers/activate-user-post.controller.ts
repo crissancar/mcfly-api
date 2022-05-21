02c36a4d521891fcc9f35bdb2c759688ae639992
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-22 00:50:45 +0200

    feat(controller): add activate user post controller

---
+++ src/modules/activated-users/controllers/activate-user-post.controller.ts
@@ -0,0 +1,29 @@
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Controller, HttpStatus, Param, Post, Res, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { UserActivator } from '../services/user-activator.service';
+import { AuthUser } from '../../auth/decorators/auth-user.decorator';
+import { AuthenticatedUser } from '../../users/dtos/authenticated-user.dto';
+import { Response } from 'express';
+import { JwtValidator } from '../../shared/services/jwt-validator.service';
+
+@ApiTags('activated-users')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@Controller('activated-users')
+export class ActivateUserPostController {
+  constructor(private activator: UserActivator) {}
+
+  @Post('activate/:id')
+  async run(@AuthUser() authUser: AuthenticatedUser, @Param('id') id: string, @Res() res: Response) {
+    try {
+      JwtValidator.verifyUserAuth(id, authUser);
+
+      await this.activator.run({ id });
+    } catch (error) {
+      res.status(error.getStatus()).send();
+    }
+
+    res.status(HttpStatus.OK).send();
+  }
+}
