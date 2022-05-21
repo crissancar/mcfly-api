c6b057d35420ef021b890f1918b8c3e7ebf783b8
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-22 00:46:29 +0200

    feat(activated-users): add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../modules/users/users.module';
 import { MongooseConfigModule } from '../config/persistence/mongoose-config.module';
 import { AuthModule } from '../modules/auth/auth.module';
+import { ActivatedUsersModule } from '../modules/activated-users/activated-users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, MongooseConfigModule],
+  imports: [UsersModule, AuthModule, ActivatedUsersModule, MongooseConfigModule],
 })
 export class AppModule {}
