939a8b48bbae4f7d850a5ec87b5ac9a31fac00b9
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-22 18:27:46 +0200

    refactor(app-module): add event emitter module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,17 @@ import { AuthModule } from '../modules/auth/auth.module';
 import { ActivatedUsersModule } from '../modules/activated-users/activated-users.module';
 import { MessagesModule } from '../modules/messages/messages.module';
 import { NotificationsModule } from 'src/modules/notifications/notifications.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [UsersModule, AuthModule, ActivatedUsersModule, MessagesModule, NotificationsModule, MongooseConfigModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ActivatedUsersModule,
+    MessagesModule,
+    NotificationsModule,
+    MongooseConfigModule,
+    EventEmitterModule.forRoot(),
+  ],
 })
 export class AppModule {}
