b8c0798d62736be906cfec11eb4ca0bbbb9c10bd
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-22 22:13:48 +0200

    feat(repository): add find method implementation

--- src/modules/notifications/persistence/mongoose-notification.repository.ts
+++ src/modules/notifications/persistence/mongoose-notification.repository.ts
@@ -1,9 +1,10 @@
-import { MongooseRepository } from 'src/modules/shared/persistence/mongoose.repository';
-import { Notifications } from './mongose-notification.model';
-import { NotificationRepository } from '../repositories/notification.repository';
-import { Model } from 'mongoose';
-import { Notification } from '../models/notification.model';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MongooseRepository } from 'src/modules/shared/persistence/mongoose.repository';
+import { NotificationRepository } from '../repositories/notification.repository';
+import { Notifications } from './mongose-notification.model';
+import { Notification } from '../models/notification.model';
+import { Nullable } from '../../shared/types/Nullable';
 
 export class MongooseNotificationRepository
   extends MongooseRepository<Notifications>
@@ -17,6 +18,12 @@ export class MongooseNotificationRepository
     const id: string = notification.id;
 
     await this.persist(id, notification);
+  }
+
+  async find(id: string): Promise<Nullable<Array<Notification>>> {
+    const notifications: any = await this.model().find({ receiverId: id });
+
+    return notifications ? Notification.fromPlainDataArray(notifications) : null;
   }
 
   protected model(): Model<Notifications> {
--- src/modules/notifications/repositories/notification.repository.ts
+++ src/modules/notifications/repositories/notification.repository.ts
@@ -1,5 +1,7 @@
+import { Nullable } from 'src/modules/shared/types/Nullable';
 import { Notification } from '../models/notification.model';
 
 export interface NotificationRepository {
   save(notification: Notification): Promise<void>;
+  find(id: string): Promise<Nullable<Array<Notification>>>;
 }
