f62cad68802f1c3054cd96f97a563b9fcffcb01e
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-22 18:18:45 +0200

    feat(notification): add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { MongooseConfigModule } from '../config/persistence/mongoose-config.modu
 import { AuthModule } from '../modules/auth/auth.module';
 import { ActivatedUsersModule } from '../modules/activated-users/activated-users.module';
 import { MessagesModule } from '../modules/messages/messages.module';
+import { NotificationsModule } from 'src/modules/notifications/notifications.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, ActivatedUsersModule, MessagesModule, MongooseConfigModule],
+  imports: [UsersModule, AuthModule, ActivatedUsersModule, MessagesModule, NotificationsModule, MongooseConfigModule],
 })
 export class AppModule {}
