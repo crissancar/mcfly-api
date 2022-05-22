595f7eb0852dc3487500180d828b947b20ab3c30
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-22 14:47:04 +0200

    feat(controller): add message get controller

---
+++ src/modules/messages/controllers/message-get.controller.ts
@@ -0,0 +1,29 @@
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, Param, Res, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { MessageFinder } from '../services/message-finder.service';
+import { AuthUser } from '../../auth/decorators/auth-user.decorator';
+import { AuthenticatedUser } from '../../users/dtos/authenticated-user.dto';
+import { Response } from 'express';
+import { JwtValidator } from '../../shared/services/jwt-validator.service';
+
+@ApiTags('messages')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@Controller('messages')
+export class MessageGetController {
+  constructor(private finder: MessageFinder) {}
+
+  @Get(':id')
+  async run(@AuthUser() authUser: AuthenticatedUser, @Param('id') id: string, @Res() res: Response) {
+    try {
+      JwtValidator.verifyUserAuth(id, authUser);
+
+      const messages = await this.finder.run({ id });
+
+      res.status(HttpStatus.OK).send(messages);
+    } catch (error) {
+      res.status(error.getStatus()).send();
+    }
+  }
+}
