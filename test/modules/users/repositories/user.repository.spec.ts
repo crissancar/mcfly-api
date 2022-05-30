6551bfd132c2e0fed918b8412e9b25948f4e6bbd
Cristian Sánchez <devcristiansanchez@gmail.com>
2022-05-30 22:00:40 +0200

    test(integration): add user repository test

---
+++ test/modules/users/repositories/user.repository.spec.ts
@@ -0,0 +1,64 @@
+import { getConnectionToken, getModelToken } from '@nestjs/mongoose';
+import { NestApplication } from '@nestjs/core';
+import { UserRepository } from 'src/modules/users/repositories/user.repository';
+import { Connection, Model } from 'mongoose';
+import { Users } from '../../../../src/modules/users/persistence/mongoose-user.model';
+import { AppModule } from '../../../../src/app/app.module';
+import { Test } from '@nestjs/testing';
+import { MongooseUserRepository } from '../../../../src/modules/users/persistence/mongoose-user.repository';
+import { UserMother } from '../mothers/user.mother';
+
+let app: NestApplication;
+let mongooseUserSchema: Model<Users>;
+let repository: UserRepository;
+
+beforeEach(async () => {
+  const module = await Test.createTestingModule({
+    imports: [AppModule],
+    providers: [
+      {
+        provide: getModelToken(Users.name),
+        useValue: Users,
+      },
+    ],
+  }).compile();
+
+  app = module.createNestApplication<NestApplication>();
+  await app.listen(3000);
+
+  mongooseUserSchema = module.get<Model<Users>>(getModelToken(Users.name));
+  repository = new MongooseUserRepository(mongooseUserSchema);
+});
+
+afterEach(async () => {
+  await (app.get(getConnectionToken()) as Connection).db.dropDatabase();
+  await app.close();
+});
+
+describe('UserRepository', () => {
+  describe('#save', () => {
+    it('should save a user', async () => {
+      const user = UserMother.random();
+
+      await repository.save(user);
+    });
+  });
+
+  describe('#findById', () => {
+    it('should find a user by id', async () => {
+      const user = UserMother.random();
+
+      await repository.save(user);
+      await repository.findById(user.id);
+    });
+  });
+
+  describe('#findByEmail', () => {
+    it('should find a user by email', async () => {
+      const user = UserMother.random();
+
+      await repository.save(user);
+      await repository.findByEmail(user.email);
+    });
+  });
+});
